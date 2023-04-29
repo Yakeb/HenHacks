1685f241b3ff18be572f4d81022fafa1fc23671e
Mike-Woods-1 <mw49576@gmail.com>
2023-04-29 16:09:48 -0400

    Our about us page

---
+++ src/AboutUs.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>About Us - The Hive</title>
+</head>
+<body>
+	<header>
+		<img src="hive_photo.jpg" alt="The Hive Team Photo" style="display:block; margin:auto;">
+	</header>
+    <main>
+	<h1>Meet The Hive:</h1>
+	<p><strong>Mission Statement:</strong> Our mission is to provide a platform for novice programmers to learn and master basic programming concepts in popular languages such as Python, Java, JavaScript, C, and C++. We aim to offer a comprehensive learning experience through our website, which includes an Integrated Development Environment (IDE) and a terminal to display user inputted code. Our goal is to educate and empower individuals who want to build a strong foundation in coding. To achieve this, we have included lessons on writing for loops, while loops, arrays, if-then statements, and syntax checkers, so that our users can gain the necessary knowledge and skills to become proficient programmers.</p>
+
+	<p><strong>Origin:</strong> We are The Hive, a team competing in HenHacks. Our team consists of Jake Milburn (left), Michael Zuppas (second from the left), Michael Woods (second from the right), and Nicholas Antrim (right). We were hired as IT interns by John Williams, the head coach of the University of Delaware Men’s Club Crew team, to work for his law firm, IncNow in Wilmington, DE. Despite our varied backgrounds and skill sets, we were able to collaborate and work together effectively on multiple projects and assignments. We quickly discovered that we had a strong ability to solve problems and work together as a team. Our reputation for successfully tackling any challenge that came our way soon followed. Inspired by the concept of a hive mind, in which social insects work together as a single entity, we decided to adopt the name "The Hive" to represent our collaborative approach to problem-solving.</p>
+    <p>Meet The Hive: Jake Milburn (Left):
+Class of 2024
+Captain of University of Delaware Men’s Club Crew
+Major: Computer Science
+Minor: Artificial Intelligence
+Semesters of Coding Experience: 6
+Fun Fact: Likes to go to the gym.
+
+Michael Zuppas (Second from the left):
+Class of 2024
+Member of University of Delaware Men’s Club Crew
+Major: Geospatial Science and Data Analytics
+Minor: N/A
+Semesters of Coding Experience: 3
+Fun Fact: Is allergic to fruit.
+
+Michael Woods (Second from the right):
+Class of 2023
+Member of University of Delaware Men’s Club Crew
+Major: Applied Mathematics
+Minor: Computer Science
+Semesters of Coding Experience: 4
+Fun Fact: Has an identical twin brother.
+
+Nicholas Antrim (Right):
+Class of 2024
+Vice President of University of Delaware Men’s Club Crew
+Major: Botany
+Minor: Computer Science
+Semesters of Coding Experience: 6
+Fun Fact: Been rowing for 8 years.
+</p>
+</main>
+</body>
+</html>
