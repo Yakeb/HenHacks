745c8ee79e377753d64bf2b1ed78f7cc7d9a8a3c
JAKEB MILBURN <MILBURNJ@UDEL.EDU>
2023-04-29 15:46:31 -0400

    changed code editor to typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import CodeEditor from './TextEditor';
+import TypeScriptEditor from './TypeScriptEditor';
 
 function App() {
   return (
     <div>
       <h1>Code Editor</h1>
-      <CodeEditor initialValue={'console.log("hello world")'}></CodeEditor>
+      <TypeScriptEditor initialValue={'console.log("hello world")'}></TypeScriptEditor>
     </div>
   );
 }
