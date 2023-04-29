5ecacc5854e0b7ddd66f6431ec0370abda772f1d
Nantrim <nantrim@udel.edu>
2023-04-29 16:21:48 -0500

    Tabs and Stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,88 @@
 import React from 'react';
 import './App.css';
+import { Tabs, Menu, Dropdown, Button } from 'antd';
+import { DownOutlined } from '@ant-design/icons';
 import TypeScriptEditor from './TypeScriptEditor';
 
+const { TabPane } = Tabs;
+
 function App() {
+  const menu = (
+    <Menu>
+      <Menu.Item key="1" onClick={() => handleMenuClick('console.log("Hello World")')}>
+        Console.log("Hello World")
+      </Menu.Item>
+      <Menu.Item key="2" onClick={() => handleMenuClick('let num: number = 5;')}>
+        Define a number variable
+      </Menu.Item>
+      <Menu.Item key="3" onClick={() => handleMenuClick('let str: string = "Hello";')}>
+        Define a string variable
+      </Menu.Item>
+      <Menu.Item key="4" onClick={() => handleMenuClick('let arr: number[] = [1, 2, 3];')}>
+        Define an array variable
+      </Menu.Item>
+      <Menu.Item key="5" onClick={() => handleMenuClick('class Car {}')}>
+        Define a class
+      </Menu.Item>
+    </Menu>
+  );
+
+  const handleMenuClick = (value:string) => {
+    setInitialValue(value);
+  };
+
+  const [initialValue, setInitialValue] = React.useState('console.log("Hello World")');
+
   return (
     <div>
-      <h1>Code Editor</h1>
-      <TypeScriptEditor initialValue={'console.log("hello world")'}></TypeScriptEditor>
+      <h1>Learn to code</h1>
+      <Tabs defaultActiveKey="1">
+        <TabPane tab={
+          <Dropdown overlay={menu} trigger={['click']}>
+            <Button>
+              TypeScript <DownOutlined />
+            </Button>
+          </Dropdown>
+        } key="1">
+          <TypeScriptEditor initialValue={initialValue} />
+        </TabPane>
+        <TabPane tab={
+          <Dropdown overlay={menu} trigger={['click']}>
+            <Button>
+              Java <DownOutlined />
+            </Button>
+          </Dropdown>
+        } key="2">
+          <p>Java editor component here</p>
+        </TabPane>
+        <TabPane tab={
+          <Dropdown overlay={menu} trigger={['click']}>
+            <Button>
+              Python <DownOutlined />
+            </Button>
+          </Dropdown>
+        } key="3">
+          <p>Python editor component here</p>
+        </TabPane>
+        <TabPane tab={
+          <Dropdown overlay={menu} trigger={['click']}>
+            <Button>
+              C++ <DownOutlined />
+            </Button>
+          </Dropdown>
+        } key="4">
+          <p>C++ editor component here</p>
+        </TabPane>
+        <TabPane tab={
+          <Dropdown overlay={menu} trigger={['click']}>
+            <Button>
+              C <DownOutlined />
+            </Button>
+          </Dropdown>
+        } key="5">
+          <p>C editor component here</p>
+        </TabPane>
+      </Tabs>
     </div>
   );
 }
--- src/TypeScriptEditor.tsx
+++ src/TypeScriptEditor.tsx
@@ -6,111 +6,105 @@ import Editor, { OnChange } from "@monaco-editor/react";
 
 
 interface CodeEditorProps {
-    initialValue: string;
-  }
-  
-  const TypeScriptEditor: React.FC<CodeEditorProps> = ({ initialValue }) => {
-    const [code, setCode] = useState(initialValue);
-    const [output, setOutput] = useState<string[]>([]);
-    const editorRef = useRef<monaco.editor.IStandaloneCodeEditor | null>(null);
-  
-    const handleCodeChange = (value: string | undefined) => {
-      if (value !== undefined) {
-        setCode(value);
-      }
-    };
-  
-    useEffect(() => {
-      if (editorRef.current) {
-        // Set up the Monaco environment
-        const uri = monaco.Uri.parse(
-          "data:text/javascript;charset=utf-8;base64," + btoa("")
-        );
-        monaco.languages.typescript.typescriptDefaults.setEagerModelSync(true);
-        monaco.languages.typescript.typescriptDefaults.addExtraLib(
-          "declare const console: { log: (...args: any[]) => void };",
-          uri.toString()
-        );
-      }
-    }, []);
-  
-    const handleCompile = () => {
-      try {
-        // Compile the code using the built-in `eval` function
-        const result = eval(code);
-  
-        // Log the output to the console-like terminal
-        if (typeof result !== "undefined") {
-          setOutput((prevOutput) => [...prevOutput, result.toString()]);
-        }
-      } catch (error) {
-        // Log the error to the console-like terminal
-        setOutput((prevOutput) => [
-          ...prevOutput,
-          `ERROR: ${(error as Error).message}`,
-        ]);
-      }
-  
-      // Log the console output to the console-like terminal
+  initialValue: string;
+}
+
+const TypeScriptEditor: React.FC<CodeEditorProps> = ({ initialValue }) => {
+  const [code, setCode] = useState(initialValue);
+  const [terminalOutput, setTerminalOutput] = useState<string[]>([]);
+  const editorRef = useRef<monaco.editor.IStandaloneCodeEditor | null>(null);
+
+  const handleCodeChange = (value: string | undefined) => {
+    if (value !== undefined) {
+      setCode(value);
+    }
+  };
+
+  useEffect(() => {
+    if (editorRef.current) {
+      const uri = monaco.Uri.parse(
+        "data:text/javascript;charset=utf-8;base64," + btoa("")
+      );
+      monaco.languages.typescript.typescriptDefaults.setEagerModelSync(true);
+      monaco.languages.typescript.typescriptDefaults.addExtraLib(
+        "declare const console: { log: (...args: any[]) => void };",
+        uri.toString()
+      );
+    }
+  }, []);
+
+  const handleCompile = () => {
+    try {
+      // Reset the console-like terminal output
+      setTerminalOutput([]);
+
+      // Save the original console.log function
+      const originalConsoleLog = console.log;
+
+      // Override the console.log function to log to the terminal output
       console.log = (...args: any[]) => {
-        setOutput((prevOutput) => [
+        setTerminalOutput((prevOutput) => [
           ...prevOutput,
           args.map((arg) => arg.toString()).join(" "),
         ]);
       };
-  
-      // Compile the code using the modified `eval` function
-      try {
-        eval(code);
-      } catch (error) {
-        // Log the error to the console-like terminal
-        setOutput((prevOutput) => [
-          ...prevOutput,
-          `ERROR: ${(error as Error).message}`,
-        ]);
+
+      // Evaluate the code using the built-in eval function
+      const result = eval(code);
+
+      // Log the result to the terminal output
+      if (typeof result !== "undefined") {
+        console.log(result);
       }
-  
-      // Reset the `console.log` function
+
+      // Reset the console.log function to its original state
       console.log = originalConsoleLog;
-    };
-  
-    const handleEditorDidMount = (
-      editor: monaco.editor.IStandaloneCodeEditor,
-      monacoRef: typeof monaco
-    ) => {
-      // Save the editor instance to a ref for later use
-      editorRef.current = editor;
-    };
-  
-    const originalConsoleLog = console.log;
-  
-    return (
+    } catch (error) {
+      // Log the error to the terminal output
+      console.log(`ERROR: ${(error as Error).message}`);
+    }
+  };
+
+  const handleClear = () => {
+    setTerminalOutput([]);
+  };
+
+  const handleEditorDidMount = (
+    editor: monaco.editor.IStandaloneCodeEditor,
+    monacoRef: typeof monaco
+  ) => {
+    editorRef.current = editor;
+  };
+
+  return (
+    <div>
+      <Editor
+        height="500px"
+        width="800px"
+        defaultLanguage="typescript"
+        defaultValue={code}
+        theme="vs-dark"
+        onChange={handleCodeChange}
+        onMount={handleEditorDidMount}
+        options={{
+          minimap: { enabled: false },
+          scrollbar: { alwaysConsumeMouseWheel: false },
+        }}
+      />
+      <button onClick={handleCompile}>Compile</button>
+      <button onClick={handleClear}>Clear</button>
       <div>
-        <Editor
-          height="500px"
-          defaultLanguage="typescript"
-          defaultValue={code}
-          theme="vs-dark"
-          onChange={handleCodeChange}
-          onMount={handleEditorDidMount}
-          options={{
-            minimap: { enabled: false },
-            scrollbar: { alwaysConsumeMouseWheel: false },
-          }}
-        />
-        <button onClick={handleCompile}>Compile</button>
-        <div>
-          <h2>Terminal:</h2>
-          {output.map((line, index) => (
-            <div key={index}>{line}</div>
-          ))}
-        </div>
+        <h2>Terminal:</h2>
+        {terminalOutput.map((line, index) => (
+          <div key={index}>{line}</div>
+        ))}
       </div>
-    );
-  };
-  
-  TypeScriptEditor.defaultProps = {
-    initialValue: "",
-  };
-  
-  export default TypeScriptEditor;
+    </div>
+  );
+};
+
+TypeScriptEditor.defaultProps = {
+  initialValue: "",
+};
+
+export default TypeScriptEditor;
