4414d339301938d66e4e16d66f434fbe9d358033
Nantrim <nantrim@udel.edu>
2023-04-30 03:56:36 -0500

    Fly and ghetto

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,46 @@
-/* eslint-disable import/no-webpack-loader-syntax */
-//import React, { useState } from 'react';
 import { useState } from 'react';
 import './App.css';
 import CSSEditor from './TextEditors/CSSEditor';
 import HTMLEditor from './TextEditors/HTMLEditor';
-//import TypeScriptEditor from './TextEditors/TypeScriptEditor';
 import UserDisplay from './UserDisplay';
 
 function App() {
-  //const initialTSText = require('!!raw-loader!./ExampleFiles/initialTS.tsx')
-  const initialHTMLText = require('!!raw-loader!./ExampleFiles/initialHTML.html')
-  const initialCSSText = require('!!raw-loader!./ExampleFiles/initialCSS.css')
+  const initialHTMLText = require('!!raw-loader!./ExampleFiles/initialHTML.html');
+  const initialCSSText = require('!!raw-loader!./ExampleFiles/initialCSS.css');
 
   const [cssCode, setCssCode] = useState(initialCSSText);
-
   const handleCssCodeChange = (value: string) => {
     setCssCode(value);
   };
-  const [htmlCode, setHtmlCode] = useState(initialHTMLText);
 
+  const [htmlCode, setHtmlCode] = useState(initialHTMLText);
   const handleHtmlCodeChange = (code: string) => {
     setHtmlCode(code);
   };
+
   return (
-      <div className="grid-container">
-        <title>Womp Nantrim Zoop Splink Mike</title>
-        <div className="grid-item">
-          <h2>HTML Editor</h2>
-          <HTMLEditor initialValue={initialHTMLText} onHtmlCodeChange={handleHtmlCodeChange} />
+    <div className="app-container">
+      <header className="app-header">
+        <h1>WebDesign101</h1>
+      </header>
+      <main className="app-main">
+        <div className="editors-container">
+          <div className="editor">
+            <h2>HTML Editor</h2>
+            <HTMLEditor initialValue={initialHTMLText} onHtmlCodeChange={handleHtmlCodeChange} />
+          </div>
+          <div className="editor">
+            <h2>CSS Editor</h2>
+            <CSSEditor cssCode={initialCSSText} onCssCodeChange={handleCssCodeChange} />
+          </div>
         </div>
-        <div className="grid-item">
-          <h2>CSS Editor</h2>
-          <CSSEditor cssCode={initialCSSText} onCssCodeChange={handleCssCodeChange} />
+        <div className="display-container">
+          <h2>Result:</h2>
+          <UserDisplay htmlCode={htmlCode} cssCode={cssCode} />
         </div>
-        <div className="grid-item">
-          <h1>User Display</h1>
-          <UserDisplay
-            htmlCode={htmlCode}
-            cssCode={cssCode}
-          ></UserDisplay>
-        </div>
-      </div>
+      </main>
+    </div>
   );
-};
+}
+
 export default App;
