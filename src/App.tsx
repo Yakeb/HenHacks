7acc2685bcaafbfd4597cdc2cdd886029295692c
JAKEB MILBURN <MILBURNJ@UDEL.EDU>
2023-04-29 21:12:47 -0400

    Jake - 2, API - 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 //import React, { useState } from 'react';
 import './App.css';
+import CSSEditor from './CSSEditor';
 import HtmlEditor from './HTMLEditor';
 import TypeScriptEditor from './TypeScriptEditor';
 
@@ -10,6 +11,8 @@ function App() {
       <TypeScriptEditor initialValue={''} />
       <h2>HTML Editor</h2>
       <HtmlEditor initialValue={''}></HtmlEditor>
+      <h3>CSS Editor</h3>
+      <CSSEditor initialValue={''}></CSSEditor>
     </div>
   );
 };
